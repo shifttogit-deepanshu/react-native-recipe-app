05a10b86f807d2035e9a61129a878a831221b71a
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-01-02 09:34:32 +0530

    Add Screens

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import AppLoading from "expo-app-loading"
+
+
+const fetchFonts = ()=>{
+  return Font.loadAsync({
+    'openSans': require('./assets/Fonts/OpenSans-Regular.ttf'),
+    'openSansBold': require('./assets/Fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
+
+  const [dataLoaded,setDataLoaded] = useState(false)
+
+  if(!dataLoaded){
+    return <AppLoading startAsync={fetchFonts} onFinish={()=>setDataLoaded(true)} onError={(err)=>console.log(err)}/>
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
