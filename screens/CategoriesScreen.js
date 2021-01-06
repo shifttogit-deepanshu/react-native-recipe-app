e9efc388c7cb3b8ba10eebc39c2965abe62e8996
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-01-06 12:33:12 +0530

    Add humburger toggler for drawer

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,14 +1,28 @@
-import React from "react"
+import React,{useLayoutEffect} from "react"
 import {View,Text,StyleSheet,Button,FlatList} from "react-native"
 import {CATEGORIES} from "../data/dummy-data"
 import CategoryGridTile from "../components/CategoryGridTile"
-
+import {
+    HeaderButtons,
+    Item,
+  } from 'react-navigation-header-buttons'
+import HeaderButton from "../components/HeaderButton"
 
 
 
 const CategoriesScreen = (props) => {
+    useLayoutEffect(()=>{
+        props.navigation.setOptions({
+            headerLeft: ()=>(
+                <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item title="drawer" iconName="ios-menu" onPress={()=>props.navigation.toggleDrawer()}/>
+                </HeaderButtons>
+            )
+        })
+    })
 
-    const renderGridItem = (itemData)=>{
+
+const renderGridItem = (itemData)=>{
         return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onSelect={()=>props.navigation.navigate('Meals',{cat:itemData.item})} style={styles.gridItem}/>
     }
 
