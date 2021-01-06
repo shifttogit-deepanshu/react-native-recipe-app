21311b3deed0cd8ec986aef7b4b6619a28519e54
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-01-06 12:44:20 +0530

    Add drawer button to filters screen

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,11 +1,26 @@
-import React from "react"
+import React,{useLayoutEffect} from "react"
 import {View,Text,StyleSheet} from "react-native"
 import MealList from "../components/MealList"
 import {MEALS} from "../data/dummy-data"
+import {
+    HeaderButtons,
+    Item,
+  } from 'react-navigation-header-buttons'
+import HeaderButton from "../components/HeaderButton"
 
 const favouriteList = MEALS.filter((meal)=>meal.id==='m1'|| meal.id==='m2')
 
 const FavouritesScreen = (props) => {
+    useLayoutEffect(()=>{
+        props.navigation.setOptions({
+            headerLeft: ()=>(
+                <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item title="drawer" iconName="ios-menu" onPress={()=>props.navigation.toggleDrawer()}/>
+                </HeaderButtons>
+            )
+        })
+    })
+
     return (
         <MealList listData={favouriteList} navigation={props.navigation} />
     )
