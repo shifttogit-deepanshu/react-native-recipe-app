0650d9fd15ab9449e37becdca7e30f569de5d606
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-01-09 03:41:28 +0530

    Add setfilter action

--- store/actions/meals.js
+++ store/actions/meals.js
@@ -1,8 +1,16 @@
 export const TOGGlE_FAVOURITE = "TOGGlE_FAVOURITE"
+export const SET_FILTERS = "SET_FILTERS"
 
 export const toggleFavourite=(id)=>{
     return {
         type:TOGGlE_FAVOURITE,
         mealId:id
     }
+}
+
+export const setFilters = (filterSettings)=>{
+    return {
+        type:SET_FILTERS,
+        filters:filterSettings
+}
 }
